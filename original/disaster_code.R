f53d31c709f7d84028393a7421f7c409407f8eb2
BelinaJang <jangbelina@gmail.com>
2024-09-16 11:59:37 -0400

    analyzing disaster data

---
+++ original/disaster_code.R
@@ -0,0 +1,12 @@
+library(here)
+library(tidyverse)
+rawdat <-read.csv(here("original","Disaster.csv"), header = TRUE)
+
+filtered_data <- rawdat %>% filter(Year %in% c(2000:2019)) %>%
+  filter(Disaster.Type %in% c("Earthquake","Drought")) %>%
+  select(Year, ISO, Disaster.Type)
+
+dummy_data <- filtered_data %>% mutate(Earthquake=ifelse(Disaster.Type=="Earthquake",1,0), Drought=ifelse(Disaster.Type=="Drought",1,0))
+
+grouped_data <- dummy_data %>% group_by(Year, ISO) %>%
+  summarise(Drought=sum(Drought),Earthquake=sum(Earthquake))
