417b00c247e947e8c80790fafbbe0177345b0851
BelinaJang <jangbelina@gmail.com>
2024-09-16 12:12:43 -0400

    fix mistake

--- original/disaster_code.R
+++ original/disaster_code.R
@@ -9,4 +9,5 @@ filtered_data <- rawdat %>% filter(Year %in% c(2000:2019)) %>%
 dummy_data <- filtered_data %>% mutate(Earthquake=ifelse(Disaster.Type=="Earthquake",1,0), Drought=ifelse(Disaster.Type=="Drought",1,0))
 
 grouped_data <- dummy_data %>% group_by(Year, ISO) %>%
-  summarise(Drought=sum(Drought),Earthquake=sum(Earthquake))
+  # 1 if it happened at least once
+  summarise(Drought=max(Drought),Earthquake=max(Earthquake))
