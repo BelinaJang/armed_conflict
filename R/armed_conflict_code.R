bb5a18b0384f527e428accff76bed43deccd6b33
BelinaJang <jangbelina@gmail.com>
2024-09-16 11:03:40 -0400

    Initial commit

---
+++ R/armed_conflict_code.R
@@ -0,0 +1,10 @@
+library(here)
+library(tidyverse)
+rawdat <-read.csv(here("original","maternalmortality.csv"), header = TRUE)
+
+data_wide <- rawdat %>% 
+  select(Country.Name,X2000:X2019)
+
+data_narrow <- pivot_longer(data_wide, X2000:X2019, names_to="Year") %>%
+  mutate(Year=as.integer(gsub("X","",Year))) %>%
+  rename(MatMor=value)
