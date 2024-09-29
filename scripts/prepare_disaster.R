40b86e52baa4ac4c5aa95b7f0cd93f14e1186f57
BelinaJang <jangbelina@gmail.com>
2024-09-29 14:36:36 -0400

    renaming tables

--- scripts/prepare_disaster.R
+++ scripts/prepare_disaster.R
@@ -2,12 +2,13 @@ library(here)
 library(tidyverse)
 rawdat <-read.csv(here("data/original","Disaster.csv"), header = TRUE)
 
-filtered_data <- rawdat %>% filter(Year %in% c(2000:2019)) %>%
+filtered_disaster <- rawdat %>% filter(Year %in% c(2000:2019)) %>%
   filter(Disaster.Type %in% c("Earthquake","Drought")) %>%
   select(Year, ISO, Disaster.Type)
 
-dummy_data <- filtered_data %>% mutate(Earthquake=ifelse(Disaster.Type=="Earthquake",1,0), Drought=ifelse(Disaster.Type=="Drought",1,0))
+dummy_data <- filtered_disaster %>% mutate(Earthquake=ifelse(Disaster.Type=="Earthquake",1,0), Drought=ifelse(Disaster.Type=="Drought",1,0))
 
-cleaned_data <- dummy_data %>% group_by(Year, ISO) %>%
+cleaned_disaster <- dummy_data %>% group_by(Year, ISO) %>%
   # 1 if it happened at least once
   summarise(Earthquake=max(Earthquake),Drought=max(Drought))
+
