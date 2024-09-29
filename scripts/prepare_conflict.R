3efb250f8020df34784c0b7bc5d381cd4c2000fe
BelinaJang <jangbelina@gmail.com>
2024-09-29 15:05:20 -0400

    update script and save data

---
+++ scripts/prepare_conflict.R
@@ -0,0 +1,12 @@
+library(here)
+library(tidyverse)
+rawdata <-read.csv(here("data/original","conflictdata.csv"), header = TRUE)
+
+cleaned_conflict <- rawdata %>% group_by(ISO, year) %>%
+  summarize(totaldeath=sum(best)) %>%
+  mutate(armed_conflict=ifelse(totaldeath>=25,1,0)) %>%
+  ungroup() %>%
+  mutate(year=year+1) %>%
+  select(-totaldeath)
+
+write.csv(cleaned_conflict, file = here("data", "analytical", "cleaned_conflict.csv"), row.names = FALSE)
