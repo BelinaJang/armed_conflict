a1ec51d9a7532fb85572796532512f85d0d2bb13
BelinaJang <jangbelina@gmail.com>
2024-09-29 14:35:47 -0400

    updating tble names

--- scripts/prepare_disaster.R
+++ scripts/prepare_disaster.R
@@ -8,6 +8,6 @@ filtered_data <- rawdat %>% filter(Year %in% c(2000:2019)) %>%
 
 dummy_data <- filtered_data %>% mutate(Earthquake=ifelse(Disaster.Type=="Earthquake",1,0), Drought=ifelse(Disaster.Type=="Drought",1,0))
 
-grouped_data <- dummy_data %>% group_by(Year, ISO) %>%
+cleaned_data <- dummy_data %>% group_by(Year, ISO) %>%
   # 1 if it happened at least once
-  summarise(Drought=max(Drought),Earthquake=max(Earthquake))
+  summarise(Earthquake=max(Earthquake),Drought=max(Drought))
